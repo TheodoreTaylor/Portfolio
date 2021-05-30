c273f70ec28b508817d098c645d5dde9d60f6eec
Bertie Taylor <taftaylor@gmail.com>
2021-05-31 00:06:12 +0200

    added index page fixes #1

--- staticsite/views.py
+++ staticsite/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(
+        request, "staticsite/index.html",
+    )
+
+
+def about(request):
+    return render(
+        request, "staticsite/about.html",
+    )
+
+
+def projects(request):
+    return render(
+        request, "staticsite/projects.html",
+    )
+
+
+def contact(request):
+    return render(
+        request, "staticsite/contact.html",
+    )
