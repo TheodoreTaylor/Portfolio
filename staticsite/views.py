40ff3122073479c79695f157f58b6658575c80ec
TheodoreTaylor <taftaylor@gmail.com>
2021-06-27 18:43:51 +0100

    added contact page fixes #8

--- staticsite/views.py
+++ staticsite/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -19,8 +22,26 @@ def projects(request):
         request, "staticsite/projects.html",
     )
 
-
 def contact(request):
-    return render(
-        request, "staticsite/contact.html",
-    )
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = "Website Inquiry"
+            body = {
+                'first_name': form.cleaned_data['first_name'],
+                'last_name': form.cleaned_data['last_name'],
+                'email': form.cleaned_data['email_address'],
+                'message': form.cleaned_data['message'],
+            }
+            message = "\n".join(body.values())
+
+            try:
+                send_mail(subject, message, 'admin@example.com', ['admin@example.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return render(
+                request, "staticsite/index.html",
+            )
+
+    form = ContactForm()
+    return render(request, "staticsite/contact.html", {'form': form})
